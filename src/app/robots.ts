04c8de730e967f6c42fde5ac03b53fc6e9144598
enginagleman193-dev <mersinyazilim1@gmail.com>
2025-12-18 02:40:53 +0300

    Add sitemap and robots for Search Console

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,8 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://mersinyazılıcd C:\Users\Deniz\Desktop\mersinyazilimm.com/sitemap.xml",
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://mersinyazılım.com/sitemap.xml",
   };
 }
